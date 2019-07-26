11802cbbccbdb981ff4c120f6132c81da496353c
Sierra Delta <sofianesk8@hotmail.com>
2019-07-26 18:13:51 +0200

    Prints all hours

--- biblihoraires.py
+++ biblihoraires.py
@@ -3,7 +3,8 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 url_libraries = "https://www.bm-lyon.fr/16-bibliotheques-et-un-bibliobus/"
-libraries_links = ["bibliotheque-du-1er/", "bibliotheque-du-2e/", "bibliotheque-du-4e-croix-rousse/", "bibliotheque-du-5e-saint-jean/", "bibliotheque-du-7e-jean-mace/", "mediatheque-de-vaise/"]
+libraries_links = ["bibliotheque-du-1er/", "bibliotheque-du-2e/", "bibliotheque-du-4e-croix-rousse/",
+                   "bibliotheque-du-5e-saint-jean/", "bibliotheque-du-7e-jean-mace/", "mediatheque-de-vaise/"]
 
 for library in libraries_links:
     html = urllib.request.urlopen(url_libraries + library).read()
@@ -15,7 +16,15 @@ for library in libraries_links:
     library_name = soup.h1.text
     print("\n" + library_name + "\n")
 
-    for i in range(7):
+    for i in range(len(libraries_links)):
         print(days[i].string + " : " + hours[i].string)
 
 print("\n")
+
+"""
+Add a way to show the current day opening hours in all libraries
+first by manual input, then by sytem datetime check
+
+Check the holiday closing dates and show if a library is closed for holidays
+Otherwise the hours will be wrong.
+"""
