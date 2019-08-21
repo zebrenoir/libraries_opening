033ca1b4556310832e092a906784e22bae66df41
Sofiane Daouzli <sofianesk8@hotmail.com>
2019-08-21 20:32:11 +0200

    implemented function version

--- biblihoraires.py
+++ biblihoraires.py
@@ -1,74 +1,48 @@
+#!/usr/bin/env python3
+
 import urllib
-import requests
 import datetime
 from bs4 import BeautifulSoup as bs
 
-url_libraries = "https://www.bm-lyon.fr/16-bibliotheques-et-un-bibliobus/"
-libraries_links = ["bibliotheque-du-1er/", "bibliotheque-du-2e/", "bibliotheque-du-4e-croix-rousse/",
-                   "bibliotheque-du-5e-saint-jean/", "bibliotheque-du-7e-jean-mace/", "mediatheque-de-vaise/"]
 
-current_day = datetime.datetime.today().weekday()
+def get_hours():
+    """
+    Add a way to show either the current day opening hours in all libraries
+    either all days in all libraries
+    first by manual input, then by sytem datetime check
 
-"""
-input_day = input("Pour quel jour de la semaine souhaitez-vous voir les horaires ?\n"
-                  "(en minuscules en français sans espaces)\n")
+    Check the holiday closing dates and show if a library is closed for holidays
+    Otherwise the hours will be wrong.
+    """
+    url_libraries = "https://www.bm-lyon.fr/16-bibliotheques-et-un-bibliobus/"
+    libraries_links = ["bibliotheque-du-1er/", "bibliotheque-du-2e/", "bibliotheque-du-4e-croix-rousse/",
+                       "bibliotheque-du-5e-saint-jean/", "bibliotheque-du-7e-jean-mace/", "mediatheque-de-vaise/"]
 
-if input_day:
-    if input_day == "lundi":
-        input_day = 0
-    elif input_day == "mardi":
-        input_day = 1
-    elif input_day == "mercredi":
-        input_day = 2
-    elif input_day == "jeudi":
-        input_day = 3
-    elif input_day == "vendredi":
-        input_day = 4
-    elif input_day == "samedi":
-        input_day = 5
-    elif input_day == "dimanche":
-        input_day = 6
-    else:
-        print("Oh boy, I'm afraid this is not working between us :'(")
-        raise SystemExit(0)
-else:
-    print("Oh come on you lazy cow!")
-    raise SystemExit(0)
+    current_day = datetime.datetime.today().weekday()
 
-# lundi = 0
-# mardi = 1
-# mercredi = 2
-# jeudi = 3
-# vendredi = 4
-# samedi = 5
-# dimanche = 6
-"""
+    libraries = []
+
+    for library in libraries_links:
+        html = urllib.request.urlopen(url_libraries + library).read()
+        soup = bs(html, "lxml")
+        table = soup.table
+        days = table.find_all("th")
+        hours = table.find_all("td")
+        holidays_raw = soup.caption.text
+        library_name = soup.h1.text
+        print("\n" + library_name + "\n")
+        lib = {
+            'name': library_name,
+            'day': days[current_day].string,
+            'hours': hours[current_day].string,
+            'holidays': holidays_raw.split("FERMETURE ")[1]
+        }
+        libraries.append(lib)
+
+    return libraries
+    # This prints all days in all libraries
+    #    for i in range(len(days)):
+    #        print(days[i].string + " : " + hours[i].string)
 
 
-for library in libraries_links:
-    html = urllib.request.urlopen(url_libraries + library).read()
-    soup = bs(html, "lxml")
-    table = soup.table
-    days = table.find_all("th")
-    hours = table.find_all("td")
-    holidays_raw = soup.caption.text
-    library_name = soup.h1.text
-    print("\n" + library_name + "\n")
 
-# This prints all days in all libraries
-#    for i in range(len(days)):
-#        print(days[i].string + " : " + hours[i].string)
-
-    print(days[current_day].string + " : " + hours[current_day].string)
-    holidays = holidays_raw.split("FERMETURE ")
-    print("Attention VACANCES !\nLa bibliothèque sera fermée " + holidays[1])
-print("\n")
-
-"""
-Add a way to show either the current day opening hours in all libraries
-either all days in all libraries
-first by manual input, then by sytem datetime check
-
-Check the holiday closing dates and show if a library is closed for holidays
-Otherwise the hours will be wrong.
-"""
--- server.py
+++ server.py
@@ -1,10 +1,7 @@
+#!/usr/bin/env python3
+
 from flask import Flask, render_template
-import jinja2
-import urllib
-import requests
-import datetime
-import lxml
-from bs4 import BeautifulSoup as bs
+import biblihoraires
 
 app = Flask(__name__)
 
@@ -16,28 +13,15 @@ def index():
 
 @app.route("/hours")
 def generate_hours():
-    url_libraries = "https://www.bm-lyon.fr/16-bibliotheques-et-un-bibliobus/"
-    libraries_links = ["bibliotheque-du-1er/", "bibliotheque-du-2e/", "bibliotheque-du-4e-croix-rousse/",
-                       "bibliotheque-du-5e-saint-jean/", "bibliotheque-du-7e-jean-mace/", "mediatheque-de-vaise/"]
 
-    current_day = datetime.datetime.today().weekday()
+    libraries = biblihoraires.get_hours()
     results = ""
-
-    for library in libraries_links:
-        html = urllib.request.urlopen(url_libraries + library).read()
-        soup = bs(html, "lxml")
-        table = soup.table
-        days = table.find_all("th")
-        hours = table.find_all("td")
-
-        library_name = soup.h1.text
-        print("\n" + library_name + "\n")
-
-        # This prints all days in all libraries
-        #    for i in range(len(days)):
-        #        print(days[i].string + " : " + hours[i].string)
-
-        results += (library_name + " : \n" + days[current_day].string + " : " + hours[current_day].string + "\n")
+    for library in libraries:
+        # results += "{name}<br/> {day} : {hours} <br/> VACANCES {holidays}<br/>".format(name=library["name"],
+        #          day=library["day"], hours=library["hours"], holidays=library["holidays"])
+        # results += "{name}<br/> {day} : {hours} <br/> VACANCES {holidays}<br/>".format(**library)
+        results += f"{library['name']}<br/> {library['day']} : {library['hours']} <br/> VACANCES {library['holidays']}<br/>"
+    # return render_template("hours.html", libraries)
 
     return results
 
