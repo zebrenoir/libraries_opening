05fdaa2e939203796d2e1a610257edad9c124e13
Sierra Delta <sierradelta.honor@gmail.com>
2019-12-25 18:07:41 +0100

    add scripts for automatic redis update for cron

---
+++ update_redis.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import biblihoraires
+
+biblihoraires.get_hours()
+
---
+++ update_redis.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /home/thecrow/libraries_opening/env/bin/activate
+/home/thecrow/libraries_opening/update_redis.py
+
