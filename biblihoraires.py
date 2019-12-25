f1aefb78de5129dd7642864f688a5671da358c15
Sierra Delta <sierradelta.honor@gmail.com>
2019-12-25 18:06:03 +0100

    stores data in redis

--- biblihoraires.py
+++ biblihoraires.py
@@ -1,22 +1,49 @@
 #!/usr/bin/env python3
 
-import urllib
-import datetime
+import json
+import urllib.request
+from datetime import datetime
 from bs4 import BeautifulSoup as bs
+import redis
 
 """
 TODO: Need to get the opening info of the whole week,
     get everyday hours and put them in an object so I can use
     what I want (even if for now I only use the current day)
 """
+def update_redis(r, data):
+    timestamp = str(datetime.timestamp(datetime.today()))
+    data = json.dumps(data)
+    r.set('data', data)
+    r.set('timestamp', timestamp)
+
 
 def get_hours():
+    r = redis.Redis(host="localhost", port=6379, db=0)
+    today = datetime.today().date()
+    timestamp = r.get('timestamp')
+    if not timestamp:
+        data = get_hours_from_website()
+        update_redis(r, data)
+    else:
+        timestamp = float(timestamp)
+        last_date = datetime.fromtimestamp(timestamp).date()
+        if last_date < today:
+            data = get_hours_from_website()
+            update_redis(r, data)
+        else:
+            data = json.loads(r.get('data'))
+
+    return data
+
+
+def get_hours_from_website():
 
     url_libraries = "https://www.bm-lyon.fr/16-bibliotheques-et-un-bibliobus/"
     libraries_links = ["bibliotheque-du-1er/", "bibliotheque-du-2e/", "bibliotheque-du-4e-croix-rousse/",
                        "bibliotheque-du-5e-saint-jean/", "bibliotheque-du-7e-jean-mace/", "mediatheque-de-vaise/"]
 
-    current_day = datetime.datetime.today().weekday()
+    current_day = datetime.today().weekday()
 
     libraries = []
 
@@ -42,12 +69,8 @@ def get_hours():
         library_name = soup.h1.text
         info_raw = soup.caption.string
         address = soup.find("div", class_="bib_adresse").string
-        try:
-            print(info_raw.encode('utf8'))
-        except:
-            pass
 
-# TODO: test if the holidays is present on the site, otherwise empty string for holidays
+        # TODO: test if the holidays is present on the site, otherwise empty string for holidays
         lib = {
             'name': library_name,
             'day': days[current_day].string,
@@ -59,6 +82,3 @@ def get_hours():
         libraries.append(lib)
 
     return libraries
-    # This prints all days in all libraries
-    #    for i in range(len(days)):
-    #        print(days[i].string + " : " + hours[i].string)
